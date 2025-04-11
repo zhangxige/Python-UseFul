d492f782cbcbddd2f88c6312ad807e98163682ac
zhangxige <daxuekanshijie@sina.cn>
2025-04-11 22:34:25 +0800

    daily commit

--- Py_Design_Pattern/Singleten.py
+++ Py_Design_Pattern/Singleten.py
@@ -39,9 +39,48 @@ class VisitEntity(threading.Thread):
         self.my_bus.sendData(self.name)
 
 
+# 使用元类
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            # 创建一个新的实例，并将其存入_instances字典中
+            cls._instances[cls] = \
+                super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+# 使用元类实现单例模式的类
+class SingletonClass(metaclass=SingletonMeta):
+    def __init__(self):
+        print("Instance created")
+
+
+# 多线程安全的单例
+class SafeSingletonMeta(type):
+    _instances = {}
+    _lock = threading.Lock()  # 锁对象用于同步
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:  # 确保线程安全
+            if cls not in cls._instances:
+                cls._instances[cls] = \
+                    super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class SafeSingletonClass(metaclass=SafeSingletonMeta):
+    def __init__(self):
+        print("Instance created")
+
+
 if __name__ == "__main__":
     for i in range(3):
         print("Entity %d begin to run..." % i)
         my_entity = VisitEntity()
         my_entity.setName("Entity_"+str(i))
         my_entity.start()
+    a = SingletonClass()
+    b = SingletonClass()
+    print(a is b)
