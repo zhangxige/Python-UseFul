ff9dd2f4ff35dca3b43bf957406a8a0ef55648c5
zhangxige <daxuekanshijie@sina.cn>
2026-04-11 21:04:21 +0800

    daily commit

--- Utils/Argparse_Utils.py
+++ Utils/Argparse_Utils.py
@@ -1,22 +1,51 @@
 import argparse
 
 
-# argparse 相关
-def argparse_test():
+# 简单的命令行计算器 demo
+def main():
     parser = argparse.ArgumentParser(
-                        prog='ProgramName',
-                        description='What the program does',
-                        epilog='Text at the bottom of help')
-    parser.add_argument('filename')           # positional argument
-    parser.add_argument('-c', '--count')      # option that takes a value
-    parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
-    parser.add_argument('--epochs', type=int, default=30)
-    parser.add_argument('--batch', type=int, default=4)
-    parser.add_argument('--exist-ok', action='store_true', help='existing key')
+        prog='Calculator',
+        description='A simple command-line calculator',
+        epilog='Example: python Argparse_Utils.py 10 5 --operation add'
+    )
+    parser.add_argument('num1', type=float, help='First number')
+    parser.add_argument('num2', type=float, help='Second number')
+    parser.add_argument('--operation',
+                         choices=['add', 'subtract', 'multiply', 'divide'],
+                         default='add',
+                         help='Operation to perform (default: add)')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
+                        help='Enable verbose output')
+
     args = parser.parse_args()
-    print(args.filename, args.count, args.verbose)
+
+    num1 = args.num1
+    num2 = args.num2
+    operation = args.operation
+
+    if operation == 'add':
+        result = num1 + num2
+        op_symbol = '+'
+    elif operation == 'subtract':
+        result = num1 - num2
+        op_symbol = '-'
+    elif operation == 'multiply':
+        result = num1 * num2
+        op_symbol = '*'
+    elif operation == 'divide':
+        if num2 == 0:
+            print("Error: Division by zero!")
+            return
+        result = num1 / num2
+        op_symbol = '/'
+
+    if args.verbose:
+        print(f"Performing: {num1} {op_symbol} {num2} = {result}")
+    else:
+        print(result)
 
 
 if __name__ == '__main__':
-    argparse_test()
-    pass
+    main()
